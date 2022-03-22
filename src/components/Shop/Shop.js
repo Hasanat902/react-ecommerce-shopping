6c91b834a933164370e49f720724198e05fb6417
Hasanat902 <abulhasanat902@gmail.com>
2022-03-22 22:41:55 +0600

    dynamic selected items added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,21 +5,33 @@ import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
+    const [cart, setCart] = useState([]);
+
     useEffect( () => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
 
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className='cart-container'>
                 <h2>Order Summary</h2>
+                <p>Selected items: {cart.length}</p>
+                <p>Total Price: $1140</p>
+                <p>Total Shipping Charge: $5</p>
+                <p>Tax: $114</p>
+                <h5>Grand Total: $1559</h5>
             </div>
         </div>
     );
