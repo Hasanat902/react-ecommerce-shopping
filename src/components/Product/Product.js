446a9d368701da009c9ff2baa5c9905e945b9b4b
Hasanat902 <abulhasanat902@gmail.com>
2022-03-22 22:25:37 +0600

    product details added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,6 +6,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Manufacturer: {seller}</small></p>
+                <p>Ratings: {ratings} stars</p>
+            </div>
+            <button className='cart-btn'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
