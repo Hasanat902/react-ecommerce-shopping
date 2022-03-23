08a3e03c7e8404963b7d313369dff85e25fd88c0
Hasanat902 <abulhasanat902@gmail.com>
2022-03-24 00:14:54 +0600

    add and load data from local storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -21,7 +21,7 @@ const Cart = (props) => {
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${shipping}</p>
             <p>Tax: ${tax}</p>
-            <h5>Grand Total: ${grandTotal}</h5>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
