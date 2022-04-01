3825fcd957e01e8484a22a727c6a85f0b032db17
Hasanat902 <abulhasanat902@gmail.com>
2022-04-01 12:26:48 +0600

    react router added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,8 +6,10 @@ const Cart = (props) => {
 
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
 
@@ -17,11 +19,12 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${shipping}</p>
             <p>Tax: ${tax}</p>
             <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+            {props.children}
         </div>
     );
 };
