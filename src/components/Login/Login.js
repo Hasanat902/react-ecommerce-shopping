7d2c332fb6cd5ad39f26e1b46c15785260f2ef6e
Hasanat902 <abulhasanat902@gmail.com>
2022-04-13 00:01:31 +0600

    added private route

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Login.css';
 import auth from '../../firebase.init';
@@ -9,6 +9,9 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
 
     const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
     
@@ -22,13 +25,14 @@ const Login = () => {
     }
 
     if(user){
-        navigate('/shop');
+        navigate(from, { replace: true });
     }
 
     const handleUserSignIn = event => {
         event.preventDefault();
 
         signInWithEmailAndPassword(email, password);
+
     }
 
     return (
